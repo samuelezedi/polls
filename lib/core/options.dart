d1ed99d442a0d045b5e90e407f11c63f098b905c
Samuel Ezedi <samuelezedi@gmail.com>
2020-05-20 08:43:33 +0100

    add core library files

---
+++ lib/core/options.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PollOptions {
+  double value;
+  String title;
+
+  PollOptions(
+      {@required this.title, @required this.value})
+      : assert(title != null),
+        assert(value != null);
+
+  show() {
+    return [this.title, this.value];
+  }
+
+}
---
+++ lib/init.dart
@@ -0,0 +1,9 @@
+
+///Polls Dart Package Initialization
+///created by twitter: @samuelezedi
+///medium https://github.com/@samuelezedi
+///if you have questions or observations, or issues
+///drop them here https://github.com/samuelezedi/polls
+
+
+export 'core/polls.dart';
